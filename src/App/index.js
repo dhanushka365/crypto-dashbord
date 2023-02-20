5b134038e82f6843dcc1808accaa39b7167b9911
dhanushka365 <technohub10000@gmail.com>
2023-02-20 19:40:32 +0530

    Add appbar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,13 @@ import './App.css';
 import Welcome from './WelcomeMessage';
 import AppLayout from './AppLayout';
 import { Component } from 'react';
+import AppBar from './AppBar';
 
 class App extends Component {
   render(){
   return (
     <AppLayout>
+      <AppBar/>
       <Welcome name={"SL Crypto Dashboard V1"}/>
     </AppLayout> 
   );
